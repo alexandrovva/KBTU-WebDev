fea3ffb7f1c79bc25c60209bfab7861781c0d8ba
Anastassiya Nikitina <47048850+alexandrovva@users.noreply.github.com>
2020-02-11 16:49:50 +0600

    Add files via upload

---
+++ To-do List/script.js
@@ -0,0 +1,21 @@
+var but = document.getElementById("button");
+var input = document.getElementById("input");
+var tasks = document.getElementById("tasks");
+
+but.addEventListener("click", add);
+
+function add(){
+    var newtask = input.value;
+        var task = `<li><input class="checkboxes" type="checkbox"><span>${newtask}</span><img src="bin.png" alt="delete"></li>`;
+        tasks.insertAdjacentHTML("beforeend", task);
+        document.getElementById("input").value = "";
+}
+
+tasks.addEventListener("click", imageClicked);
+
+function imageClicked(){
+    var task = event.target;
+    if (task.tagName === "IMG") {
+        tasks.removeChild(task.parentNode);
+    }
+}
